9cb1ff9a517985af3473c0eeaad64b48429ff88a
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-05-20 10:20:14 -0400

    Created the basic App component

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <header>
+        <h1>Video Chat with Hooks</h1>
+      </header>
+      <main>
+        <p>VideoChat goes here.</p>
+      </main>
+      <footer>
+        <p>
+          Made with{' '}
+          <span role="img" aria-label="React">
+            ⚛
+          </span>{' '}
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
